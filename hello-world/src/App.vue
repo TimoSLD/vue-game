16888db3f31fd79bb263ad581a37ed28637daff9
Timo Lemmen <timo@intermixmanagement.nl>
2023-09-06 14:57:44 +0200

    begin

---
+++ hello-world/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <h1>hoi</h1>
+  <section class="game-board">
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+  </section>
+</template>
+
+<script>
+import Card from './components/Card'
+export default {
+  name: 'App',
+  components: {
+    Card
+  },
+  data() {
+    
+  }
+}
+</script>
+
+<style>
+#app {
+
+}
+
+.card {
+  border: 5px solid #ccc
+}
+
+.game-board {
+  display: grid;
+  grid-template-columns: repeat(4, 100px);
+  grid-template-rows: repeat(4, 100px);
+  grid-column-gap: 30px;
+  grid-row-gap: 30px;
+  justify-content: center;
+}
+</style>
---
+++ hello-world/src/components/Card.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+  <div class="card">
+      card
+  </div>
+</template>
+
+<style>
+
+</style>
